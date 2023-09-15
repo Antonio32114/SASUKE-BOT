31d64276aa7892221be4ea046a07af250a546c5c
Antonio <110497758+Antonio32114@users.noreply.github.com>
2023-09-15 00:13:39 -0500

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,14 +1,10 @@
-function handler(m) {
-  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), estilo, { contextInfo: { externalAdReply: { showAdAttribution: true }}})
-
-
-}
-
-handler.help = ['creador']
-handler.tags = ['info']
-
-handler.command = ['owner', 'creator', 'creador', 'dueño'] 
-
-
-export default handler
+function handler(m) { 
+   const data = global.owner.filter(([id, isCreator]) => id && isCreator) 
+   this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m) 
+ } 
+ handler.help = ['creador', 'programador'] 
+ handler.tags = ['info'] 
+  
+ handler.command = /^(owner)$/i 
+  
+ export default handler
