2883afb608f29b100548cac49d9d7e22a81f7abd
Antonio <110497758+Antonio32114@users.noreply.github.com>
2023-09-15 00:04:42 -0500

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,19 +1,14 @@
-let handler = async (m, { conn }) => {
-try {
-let pp = imagen4
-let img =  './lib/img/grupo.jpg'
-let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let str = `*OWNER*
+function handler(m) {
+  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), estilo, { contextInfo: { externalAdReply: { showAdAttribution: true }}})
 
-• *𝗛ᴏʟᴀ,➟${taguser}* ${ANTONIO}`.trim() 
-if (m.isGroup) {
-      conn.sendMessage(m.chat, {text: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.packname, "containsAutoReply": true, "mediaType": 1, "thumbnail": pp, "mediaUrl": `https://atom.bio/killuabot649`, "sourceUrl": `https://atom.bio/killuabot649`}}}, {quoted: m});
-    } else {
-      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
-      conn.sendMessage(m.chat, {text: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.packname, "containsAutoReply": true, "mediaType": 1, "thumbnail": pp, "mediaUrl": `https://atom.bio/killuabot649`, "sourceUrl": `https://atom.bio/killuabot649`}}}, {quoted: fkontak2});
-    }
-  } catch {
-  }
-};
-handler.command = /^(owner|creator|creador|propietario)$/i
+
+}
+
+handler.help = ['creador']
+handler.tags = ['info']
+
+handler.command = ['owner', 'creator', 'creador', 'dueño'] 
+
+
 export default handler
